8fddeb198c1137591bc4c7f8d36bfa97803b89c2
Y AM <yam532004gmail.com>
2023-11-23 00:48:20 +0700

    update

--- result.php
+++ result.php
@@ -2,28 +2,74 @@
 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiA12yd2RmUTc0L5Z52ACzkZuKVc4ko8kdMDcKni2loBDHhzgHGKHkXnZgMxfhOoUwIRk&usqp=CAU" alt="Huan chuong">
 <h1>Thx for <?php if(isset($_POST["name"])) { echo $_POST["name"]; } ?> !!</h1>
 
-<p>We received your application for the <?php if(isset($_POST["club"])) { echo $_POST["club"]; } ?></p><br>
+<p>We received your application for the <?php if(isset($_POST["club"])) { echo $_POST["club"]; } ?></p>
 
-<p>You will be avaliable on <?php if(isset($_POST["time"])) { echo $_POST["time"]; } ?></p><br>
+<p>You are 
+<?php
+$printAnd = false; // Biến trung gian để kiểm tra xem có nên in "and" hay không
 
-<p>
-    <?php if(isset($_POST["thebestcoder"])) { echo $_POST["thebestcoder"]." and ";}?>
+if (isset($_POST["thebestcoder"])) {
+    echo $_POST["thebestcoder"];
+    $printAnd = true;
+}
 
-    <?php if(isset($_POST["asuperstar"])) { echo $_POST["asuperstar"] ." and ";} ?>
+if (isset($_POST["asuperstar"])) {
+    if ($printAnd) {
+        echo " and ";
+    }
+    echo $_POST["asuperstar"];
+    $printAnd = true;
+}
 
-    <?php if(isset($_POST["singer"])) { echo $_POST["singer"]." and ";} ?>
+if (isset($_POST["singer"])) {
+    if ($printAnd) {
+        echo " and ";
+    }
+    echo $_POST["singer"];
+    $printAnd = true;
+}
 
-    <?php if(isset($_POST["thebesteater"])) { echo $_POST["thebesteater"]." and "; } ?>
+if (isset($_POST["thebesteater"])) {
+    if ($printAnd) {
+        echo " and ";
+    }
+    echo $_POST["thebesteater"];
+    $printAnd = true;
+}
 
-    <?php if(isset($_POST["thebesteater"])) { echo $_POST["thebesteater"]." and "; } ?>
+if (isset($_POST["goodinarts"])) {
+    if ($printAnd) {
+        echo " and ";
+    }
+    echo $_POST["goodinarts"];
+    $printAnd = true;
+}
 
-    <?php if(isset($_POST["goodinarts"])) { echo $_POST["goodinarts"]." and "; } ?>
+if (isset($_POST["acrazydancer"])) {
+    if ($printAnd) {
+        echo " and ";
+    }
+    echo $_POST["acrazydancer"];
+    $printAnd = true;
+}
 
-    <?php if(isset($_POST["acrazydancer"])) { echo $_POST["acrazydancer"]." and "; } ?>
+if (isset($_POST["goodindesign"])) {
+    if ($printAnd) {
+        echo " and ";
+    }
+    echo $_POST["goodindesign"];
+    $printAnd = true;
+}
 
-    <?php if(isset($_POST["goodindesign"])) { echo $_POST["goodindesign"]." and "; } ?>
+if (isset($_POST["goodinspeeches"])) {
+    if ($printAnd) {
+        echo " and ";
+    }
+    echo $_POST["goodinspeeches"];
+}
 
-    <?php if(isset($_POST["goodinspeeches"])) { echo $_POST["goodinspeeches"]; } ?>
+?>
 </p>
+<p>You will be avaliable on <?php if(isset($_POST["time"])) { echo $_POST["time"]; } ?></p><br>
 </div>
 
